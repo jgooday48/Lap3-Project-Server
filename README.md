8132c9583b6a17cf1377634884813d24c317ec4d
jgooday48 <joelgooday48@gmail.com>
2023-12-15 13:51:28 +0000

    add readme and app.js file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Lap3-Project-Server
+
+## Installation
+
+- Clone or fork this repo
+- Run `npm i` to install required packages
+- Run `touch .env`
+- Copy and paste `PORT=3000` and `MONGO_ARI=DB_URL` into the .env file
+- Visit the Mongo DB website (https://www.mongodb.com/) and set up a db instance
+- Replace `DB_URL` with the URL of your mongo db instance
+- Run `npm run dev` to start the server
+
+## Technologies used
+- Mongoose 
+- Mongo DB
+- JavaScript
+- Express
+- NoSQL
---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Routes = require('./routes/')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+app.use('/', Routes)
+
+mongoose.connect(process.env.MONGO_URI) //connect mongoose db
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`API listening on Port ${process.env.PORT}`);
+        })
+        
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+app.get('/', (req,res) => {
+    res.json({message: "welcome"})
+})
+
+
+module.exports = app
