46e3a861f5b436a390814ee0d492acde48032e77
gajani-yoga <jeyagajani@hotmail.co.uk>
2023-12-18 10:52:34 +0000

    add user model Schema

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const Routes = require('./routes/')
+const userRoutes = require('./routers/users')
 
 const app = express()
 app.use(express.json())
@@ -13,6 +14,7 @@ app.use(cors())
 
 
 app.use('/', Routes)
+
 
 mongoose.connect(process.env.MONGO_URI) //connect mongoose db
     .then(() => {
@@ -28,7 +30,11 @@ mongoose.connect(process.env.MONGO_URI) //connect mongoose db
 
 app.get('/', (req,res) => {
     res.json({message: "welcome"})
+
+
 })
+
+
 
 
 module.exports = app
