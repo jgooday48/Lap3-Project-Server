9fa9561ea126c3398017ab5142e84a9a920ac806
Khider <khider.tariq1@gmail.com>
2023-12-19 12:54:13 +0000

    fixes update user info, in controller

--- controllers/users.js
+++ controllers/users.js
@@ -82,7 +82,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // route PATCH /users/profile
 // @access private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    const user  =  await User.findById(req.user._id);
+    const user  =  await User.findById(req.body._id);
 
     if(user){
         user.name = req.body.name || user.name;
